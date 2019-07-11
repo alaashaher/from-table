0bd98f911dac739eb745570be3c9f0c03e196aa8
alaa shaher <alaashaher12@gmail.com>
2019-07-11 10:13:48 +0200

    create table

--- src/form/index.js
+++ src/form/index.js
@@ -1,67 +1,64 @@
 import React, { useState } from 'react';
 import './index.css'
+import { Table, Button, Input } from "reactstrap";
+
 const Form = () => {
-  const [values, setValues] = useState({});
-  let jesonValue =[{name:'',counatary:''}];
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    jesonValue.push(values);
-    console.log(jesonValue);
+  const [rows, setRows] = useState([{}]);
+  const handleChange = index => e => {
+    const { name, value } = e.target;
+    // const rows = [...rows];
+    rows[index] = {
+      [name]: value
+    };
+    setRows(rows);
+    console.log(rows);
   };
-
-  const handleChange = (event) => {
-    event.persist();
-    setValues(values => ({ ...values, [event.target.name]: event.target.value }));
+  const handleAddRow = () => {
+    const item = {
+      name: "",
+      select: ""
+    };
+    setRows([...rows, item]);
+    console.log(rows);
   };
-  function login() {
-    console.log(values);
-  }
-
-  // const [rows, setRows] = useState([{}]);
-  // const handleRemoveRow = () => {
-  //     setRows(rows.slice(0, -1));
-  // };
-
+  const handleRemoveRow = () => {
+    setRows(rows.slice(0, -1));
+    console.log(rows);
+  };
   return (
-    <div className="section is-fullheight">
-      <div className="container">
-        <div className="column">
-          <div className="box">
-            <form onSubmit={handleSubmit}>
-              <div className="field">
-                <label className="label">Name</label>
-                <div className="control">
-                  <input className="input" type="name" name="name" required onChange={handleChange} value={values.name} />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label">countary</label>
-                <div className="control">
-                  <select onChange={handleChange} value={values.select} name="select">
-                    <option value="egypt">egypt</option>
-                    <option value="US">US</option>
-                    <option value="UK">UK</option>
-                    <option value="Dubi">Dubi</option>
-                  </select>
-                </div>
-              </div>
-              <button type="submit" className="btn btn-dark submit">Add Row</button>
-            </form>
-            <p>user name is: {values.name}<br></br>  user countary is: {values.select}</p>
-          </div>
-          <table id='students'>
-            <tbody>
-              <tr>
-                <td >{values.name}</td>
-                <td >{values.select}</td>
-                <td>
-                  <button type="" className="btn">Delete Row</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div>
+      <Table>
+        <tbody>
+          {rows.map((item, index) => (
+            <tr id="addr" key={index}>
+              <td>{index}</td>
+              <td>
+                <Input
+                  type="text"
+                  name="name"
+                  value={rows[index].name}
+                  onChange={handleChange(index)}
+                />
+              </td>
+              <td>
+                <select onChange={handleChange(index)} value={rows[index].select} name="select">
+                  <option value="egypt">egypt</option>
+                  <option value="US">US</option>
+                  <option value="UK">UK</option>
+                  <option value="Dubi">Dubi</option>
+                </select>
+              </td>
+              <td>
+                <Button color="danger" onClick={handleRemoveRow}>
+                  Delete Last Row
+            </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Button onClick={handleAddRow}>Add Row</Button>
+
     </div>
   );
 };
